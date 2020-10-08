c4c4cdc7b2937dd5e38f6a70d37dd1483dab787b
eyobell-berhe <eyobell@sourcetoad.com>
2020-10-07 21:51:13 -0400

    Adding error message

--- perlman-clinic-interview/src/Components/ContactForm/ContactForm.js
+++ perlman-clinic-interview/src/Components/ContactForm/ContactForm.js
@@ -1,54 +1,103 @@
 import React, { Component } from 'react';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import { validateEmail } from '../../Modules/form-validation';
 
 class ContactForm extends Component {
-  state = {
-    name: '',
-    email: '',
-    gender: '',
-    textarea: ''
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formFields: {
+        name: '',
+        email: '',
+        gender: '',
+        textarea: ''
+      },
+      errors: [],
+      validated: false
+    };
+
+    this.getValidationErrors = this.getValidationErrors.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+    
+  }
 
   handleInputChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
   setGender(e) {
-    this.setState({ gender: e.target.value});
+    this.setState({ gender: e.target.value });
   }
 
-  submitForm(e){
+  getValidationErrors() {
+    const errorList = [];
+
+    for (let [key, value] of Object.entries(this.state.formFields)) {
+      if (!value) {
+        errorList.push(key);
+      }
+    }
+
+    // Validate emails && returns proper error message
+    if (this.state.email && !validateEmail(this.state.email)) {
+      errorList.push('Please enter a valid email address');
+    }
+    console.log(this.state.errors);
+    this.setState({ errors: errorList, validated: true });
+    //   console.log(errorList.length > 0 ? errorList : undefined);
+  }
+
+  submitForm(e) {
     e.preventDefault();
-      alert(`submitted form for ${this.state.name}`)
+    this.setState({
+      errors: [],
+      validated: false
+    });
+    this.getValidationErrors();
   }
   render() {
-    console.log(this.state);
     return (
-      <div style={{}}>
+      <div>
         <h1> Contact Form </h1>
 
-        <form onSubmit={this.submitForm}>
+        <form>
+          {this.state.formFields.name.length === 0 && this.state.validated ? (
+            <ErrorMessage name="name" errors={this.state.errors} />
+          ) : null}
           <label>
             Name:
-            </label>
             <input
               type="text"
               name="name"
               onChange={e => this.handleInputChange(e)}
             />
+          </label>
+          {!this.state.formFields.email && this.state.validated ? (
+            <ErrorMessage name="email" errors={this.state.errors} />
+          ) : null}
           <label>
-            email:
+            Email:
             <input
               type="email"
               name="email"
               onChange={e => this.handleInputChange(e)}
             />
+            {!this.state.formFields.gender && this.state.validated ? (
+              <ErrorMessage name="gender" errors={this.state.errors} />
+            ) : null}
           </label>
           <div onChange={e => this.setGender(e)}>
             <input type="radio" value="Male" name="gender" /> Male
             <input type="radio" value="Female" name="gender" /> Female
           </div>
-          <textarea>Textarea...</textarea>
-          <input type="submit" />
+          {!this.state.formFields.textarea && this.state.validated ? (
+            <ErrorMessage name="textarea" errors={this.state.errors} />
+          ) : null}
+          <textarea onChange={e => this.handleInputChange(e)} name="textarea">
+            Textarea...
+          </textarea>
+          <button onClick={this.submitForm}>Submit</button>
         </form>
       </div>
     );
---
+++ perlman-clinic-interview/src/Components/ErrorMessage/ErrorMessage.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+
+const ErrorMessage = () => {
+    return(
+        <div style={{color:'red'}}> Please fill out the missing field. </div>
+    )
+}
+export default ErrorMessage; 
